5c2b2e670a6386935e4dbbd9caaa31c9c2864d21
stac3y <stacy.starina@gmail.com>
2021-09-25 20:30:32 +0300

    feat: user model

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const User = sequelize.define('user', {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+    },
+    {
+        paranoid: true,
+        timestamps: true,
+        updatedAt: false
+    }
+);
+
+module.exports = User;
