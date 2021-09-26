bea4b6d995b714ee5c5cc50f33c07ebe9e6fd389
stac3y <stacy.starina@gmail.com>
2021-09-26 11:28:57 +0300

    fix: add database username to .env

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 require('custom-env').env('staging');
 
-const sequelize = new Sequelize('postgres', 'postgres', process.env.POSTGRES_PASSWORD, {
+const sequelize = new Sequelize('postgres', process.env.POSTGRES_USERNAME, process.env.POSTGRES_PASSWORD, {
     dialect: 'postgres',
     host: 'localhost',
     schema: 'rest_api'
