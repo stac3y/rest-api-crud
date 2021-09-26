6ad1c0b51182f59ab90aa6920eb59cee114e1b51
stac3y <stacy.starina@gmail.com>
2021-09-26 12:49:57 +0300

    feat: add get request for getting all users

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 
+//create a new user
 exports.createUser = (req, res, next) => {
     const name = req.body.name;
     const email = req.body.email;
@@ -12,9 +13,21 @@ exports.createUser = (req, res, next) => {
     })
         .then(user => {
             res.status(201).json({
-                message: 'User created successfully!',
+                message: 'User created successfully.',
                 user: user
             })
+        })
+        .catch(err => console.log(err));
+}
+
+//get all not deleted users
+exports.getUsers = (req, res, next) => {
+    User.findAll({where: {deletedAt: null}})
+        .then(users => {
+            res.status(200).json({
+                message: 'Users fetched successfully.',
+                users: users
+            });
         })
         .catch(err => console.log(err));
 }
--- routes/user.js
+++ routes/user.js
@@ -7,4 +7,7 @@ const router = express.Router();
 //POST /user/create-user
 router.post('/create-user', userController.createUser);
 
+//GET /user/users
+router.get('/users', userController.getUsers);
+
 module.exports = router;
