fded81b23d80ccfc5644c9624fd1854761de07f3
stac3y <stacy.starina@gmail.com>
2021-09-27 19:14:11 +0300

    fix: fix request path for getting user by name

--- routes/user.js
+++ routes/user.js
@@ -5,22 +5,22 @@ const validationHandler = require('../middleware/validation');
 
 const router = express.Router();
 
-//POST /user/create-user
+//POST /users
 router.post('/users', validationHandler, userController.createUser);
 
-//GET /user/users
+//GET /users
 router.get('/users', userController.getUsers);
 
-//GET /user/id/:userId
+//GET /users/id/:userId
 router.get('/users/id/:userId', userController.getUserById);
 
-//GET /user/name/:userName
-router.get('users/name/:userName', userController.getUserByName);
+//GET /users/name/:userName
+router.get('/users/name/:userName', userController.getUserByName);
 
-//PUT /user/update-user/:userId
+//PUT /users/:userId
 router.put('/users/:userId', validationHandler, userController.updateUser);
 
-//DELETE /user/update-user/:userId
+//DELETE /users/:userId
 router.delete('/users/:userId', userController.deleteUser);
 
 module.exports = router;
